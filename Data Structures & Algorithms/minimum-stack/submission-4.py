9d4b75974711c0e35da010a6b7caecdf8a291885
caner türkmen <emoty88@gmail.com>
2026-04-21 22:23:24 -0700

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.s = []
+        self.mins = []
+        
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+        if len(self.mins) == 0:
+            self.mins.append(val)
+        else:
+            self.mins.append(min(self.mins[-1], val))
+        
+
+    def pop(self) -> None:
+        poped = self.s.pop()
+        self.mins.pop()
+        
+
+    def top(self) -> int:
+        return self.s[-1]
+        
+
+    def getMin(self) -> int:
+        return self.mins[-1]
+        
