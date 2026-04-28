5daf5c5a4d80981cae4211de5f94292b208cc5ac
caner türkmen <emoty88@gmail.com>
2026-04-27 22:35:13 -0700

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed), reverse=True)
+        fleets=[]
+        for p,s in cars:
+            t = (target - p) / s
+            if not fleets or fleets[-1] < t:
+                fleets.append(t)
+        
+        return len(fleets)
